fd10320bfa07d86d454d11bbc0c4895e81eeb680
isaac2206 <isaac-lopes@hotmail.com>
2021-02-04 15:13:28 -0300

    colocando um quadrado no hmtl4

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
-<h1>Tá Pra Vencer!</h1>
-
 <h3> Seu ajudante no controle da dispensa! </h3>
 
 <div style="display:flex">
 	<div style="width:200px;height:200px;background:green;color:white;padding:50px;margin:10px">
   	<h3>Menos desperdício!<h3>
+    <br>
+    <img arc="i1.jfif" style="width: 200px">
 	</div>
 
 
 	<div style="width:200px;height:200px;background:yellow;padding:50px;margin:10px">
   	<h3>Mais economia!<h3>
+    <br>
+    <img arc="i2.jfif" style="width: 200px">
 	</div>
 </div>
-
